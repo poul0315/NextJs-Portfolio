f13634bf1fd06147653252f231b1c1ceb729ee68
Paul Nam <poul0315@gmail.com>
2023-06-01 11:11:55 -0700

    feat(app): Adjusted profile picture and self-introduction for better UI

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Logo() {
   return (
-    <svg id="logo">
-      <circle cx="20" cy="20" r="15" stroke="blue" stroke-width="4" />
-      Sorry, your browser does not support inline SVG.
-    </svg> 
-    // <div>Paul</div>
+    // <svg id="logo">
+    //   <circle cx="20" cy="20" r="15" stroke="blue" strokeWidth="4" />
+    //   Sorry, your browser does not support inline SVG.
+    // </svg> 
+    <div id='logo'>Paul Nam</div>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '@/sections/Navbar'
+import Hero from '@/sections/Hero'
 import SocialIcons from '@/components/SocialIcons'
 import Email from '@/components/Email'
 
@@ -7,6 +8,7 @@ function Index() {
   return (
     <div className="app">
       <Navbar />
+      <Hero />
       <SocialIcons />
       <Email />
     </div>
