31c6081f7581a761612074651e6199bfb3fbfa63
Paul Nam <poul0315@gmail.com>
2023-06-04 00:49:33 -0700

    feat(app): Added smooth react scroll to Navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,31 +1,42 @@
-import Link from "next/link";
+// import Link from "next/link";
 import React from "react";
+import { Link } from 'react-scroll';
 import Logo from "@/components/Logo";
 import Button from "@/components/Button";
 
-
 function Navbar() {
     const sectionLinks = [
-        { name: "About", link: "/#about" },
-        { name: "Experience", link: "/#experience" },
-        { name: "Project", link: "/#project" },
-        { name: "Contact", link: "/#contact" },
+        { name: "About", link: "about" },
+        { name: "Experience", link: "experience" },
+        { name: "Project", link: "project" },
+        { name: "Contact", link: "contact" },
     ];
 
     return (
         <nav>
             <div className="wrapper">
                 <div className="brand">
-                    <Link href="/">
+                    <Link activeClass="active" to="logo" spy={true} smooth={true} offset={50} duration={500}>
                         <Logo />
                     </Link>
                 </div>
                 <div className="nav-items">
                     <ul className="nav-items-list">
                         {
-                            sectionLinks.map(({name,link}) => (
+                            sectionLinks.map(({ name, link }) => (
                                 <li key={name} className="nav-items-list-item">
-                                    <Link href={link} className="nav-items-list-item-link">{name}</Link>
+                                    <Link 
+                                        activeClass="active"
+                                        to={link}
+                                        spy={true}
+                                        smooth={true}
+                                        offset={60}
+                                        duration={700}
+                                        // href={link} 
+                                        className="nav-items-list-item-link"
+                                    >
+                                        {name}
+                                    </Link>
                                 </li>
                             ))
                         }
