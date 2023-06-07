9407362984ed7eb45aeeb1cf5a6acfc1da1d2c8b
Paul Nam <poul0315@gmail.com>
2023-06-06 22:39:08 -0700

    feat(app): Added work experiences details in experience component

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,33 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function Experience() {
+    const [selected, setSelected] = useState(0);
+
+    const experiences = [
+        {
+            name: "Qualitel",
+            role: "Electrical Test Engineer",
+            url: "https://qualitel.com/",
+            positionDescription: [
+                "Collaborated with a senior engineer to develop a LabView test fixture program, referring to Python3 code.",
+                "Designed test framework components, such as the user interface, process models, and step types.",
+                "Created test sequences within the established framework.",
+                "Troubleshot and resolved issues encountered during the execution of tests in a production environment."
+            ],
+        },
+        {
+            name: "Paccar",
+            role: "Electrical Design Engineer",
+            url: "https://paccar.com/",
+            positionDescription: [
+                "Collaborated with a senior engineer to develop a LabView test fixture program, referring to Python3 code.",
+                "Designed test framework components, such as the user interface, process models, and step types.",
+                "Created test sequences within the established framework.",
+                "Troubleshot and resolved issues encountered during the execution of tests in a production environment."
+            ],
+        }
+    ];
+
     return (
         <div className="experience" id="experience">
-            <Image 
-                src="/profile-picture.png" 
-                className="hero-profile-image"
-                width={200}
-                height={200}
-                alt="picture of Paul"
-            />
-            <h1 className="hero-title">
-                Hi my name is
-            </h1>
-            <h2 className="hero-title-large">
-                Paul Nam
-            </h2>
-            <h3 className="hero-title-large hero-title-sub">
-                I love programming.
-            </h3>
-            <p className="hero-text">
-                I&apos;m an electrical engineer with expereince in web developing
-                and embedded coding in C++. My current interest is learning new technology
-                and expand my knowledge. As I'm an electrical engineer, also interested in 
-                designing PCB and Microcontroller system.
-            </p>
+            <div className="title">
+                <h2>Work Experience</h2>
+            </div>
+            <div className="container">
+                <ul className="exp-slider">
+                    <div className="underline"></div>
+                    {experiences.map((experience, index) => {
+                        return (
+                          <li key={index} className={`exp-slider-item ${
+                            index === selected && "exp-slider-item-selected"}`}
+                            onClick={() => setSelected(index)}
+                            >  
+                            <span>{experience.name}</span>
+                          </li>
+                        );
+                    })};
+                </ul>
+                <div className="exp-details">
+                    <div className="exp-details-position">
+                        <h3>
+                            <span>{experiences[selected].role}</span>
+                            <span className="exp-details-position-company">
+                                &nbsp;@&nbsp;
+                                <Link className="link" href={experiences[selected].url}>
+                                    {experiences[selected].name}{""}
+                                </Link>
+                            </span>
+                        </h3>
+                        <ul className="exp-details-list">
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Experience;
