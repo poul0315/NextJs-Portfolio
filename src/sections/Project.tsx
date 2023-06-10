1fefb22ff391a951f8f760087a15ae14cb08abdb
Paul Nam <poul0315@gmail.com>
2023-06-09 19:21:07 -0700

    feat(app): Modified and added project title to project component

--- src/sections/Project.tsx
+++ src/sections/Project.tsx
@@ -1,32 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 function Project() {
+    const [selected, setSelected] = useState(0);
+
     return (
-        <div className="project" id="project">
-            <Image 
-                src="/profile-picture.png" 
-                className="hero-profile-image"
-                width={200}
-                height={200}
-                alt="picture of Paul"
-            />
-            <h1 className="hero-title">
-                Hi my name is
-            </h1>
-            <h2 className="hero-title-large">
-                Paul Nam
-            </h2>
-            <h3 className="hero-title-large hero-title-sub">
-                I love programming.
-            </h3>
-            <p className="hero-text">
-                I&apos;m an electrical engineer with expereince in web developing
-                and embedded coding in C++. My current interest is learning new technology
-                and expand my knowledge. As I'm an electrical engineer, also interested in 
-                designing PCB and Microcontroller system.
-            </p>
+        <div className="propject" id="project">
+            <div className="title">
+                <h2>Project</h2>
+            </div>
         </div>
+
     )
 }
 
