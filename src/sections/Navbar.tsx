57abf0bdf0e1e96faf5b5dbca006c4c45e80580c
Paul Nam <poul0315@gmail.com>
2023-06-10 23:49:50 -0700

    added responsive navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,10 +1,33 @@
 // import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from 'react-scroll';
 import Logo from "@/components/Logo";
 import Button from "@/components/Button";
 
 function Navbar() {
+    useEffect(() => {
+        const navbarToggle = document.querySelector("#navbar-toggle");
+        const navbarMenu = document.querySelector("#navbar-menu");
+        const navbarLinksContainer = navbarMenu?.querySelector(".navbar-links");
+        let isNavbarExpanded = navbarToggle?.getAttribute("aria-expanded") === "true";
+
+        const toggleNavbarVisibility = () => {
+        isNavbarExpanded = !isNavbarExpanded;
+        navbarToggle?.setAttribute("aria-expanded", isNavbarExpanded.toString());
+        };
+
+        navbarToggle?.addEventListener("click", toggleNavbarVisibility);
+
+        navbarLinksContainer?.addEventListener("click", (e) => e.stopPropagation());
+        navbarMenu?.addEventListener("click", toggleNavbarVisibility);
+        return () => {
+            navbarToggle?.removeEventListener('click', toggleNavbarVisibility); // Add null check using '?'
+            navbarLinksContainer?.removeEventListener('click', (e) => e.stopPropagation()); // Add null check using '?'
+            navbarMenu?.removeEventListener('click', toggleNavbarVisibility); // Add null check using '?'
+        };
+    }, []);
+
+
     const sectionLinks = [
         { name: "About", link: "about" },
         { name: "Experience", link: "experience" },
@@ -13,39 +36,60 @@ function Navbar() {
     ];
 
     return (
-        <nav>
-            <div className="wrapper">
-                <div className="brand">
-                    <Link activeClass="active" to="logo" spy={true} smooth={true} offset={50} duration={500}>
-                        <Logo />
-                    </Link>
+        // <nav>
+        //     <div className="wrapper">
+        //         {/* <div className="brand">
+        //             <Link activeClass="active" to="logo" spy={true} smooth={true} offset={50} duration={500}>
+        //                 <Logo />
+        //             </Link>
+        //         </div> */}
+        //         <div className="nav-items">
+        //             <ul className="nav-items-list">
+        //                 {
+        //                     sectionLinks.map(({ name, link }) => (
+        //                         <li key={name} className="nav-items-list-item">
+        //                             <Link 
+        //                                 activeClass="active"
+        //                                 to={link}
+        //                                 spy={true}
+        //                                 smooth={true}
+        //                                 offset={-300}
+        //                                 duration={700}
+        //                                 className="nav-items-list-item-link"
+        //                             >
+        //                                 {name}
+        //                             </Link>
+        //                         </li>
+        //                     ))
+        //                 }
+        //             </ul>
+        //             <div className="nav-items-button">
+        //                 <Button text="Resume" link="http://localhost:3000/resume.pdf" />
+        //             </div>
+        //         </div>
+        //     </div>
+        // </nav>
+        <header id="navbar">
+            <nav className="navbar-container container">
+                <a href="/" className="home-link">
+                <div className="navbar-logo"></div>
+                Website Name
+                </a>
+                <button type="button" id="navbar-toggle" aria-controls="navbar-menu" aria-label="Toggle menu" aria-expanded="false">
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                </button>
+                <div id="navbar-menu" aria-labelledby="navbar-toggle">
+                <ul className="navbar-links">
+                    <li className="navbar-item"><a className="navbar-link" href="/about">About</a></li>
+                    <li className="navbar-item"><a className="navbar-link" href="/blog">Blog</a></li>
+                    <li className="navbar-item"><a className="navbar-link" href="/careers">Careers</a></li>
+                    <li className="navbar-item"><a className="navbar-link" href="/contact">Contact</a></li>
+                </ul>
                 </div>
-                <div className="nav-items">
-                    <ul className="nav-items-list">
-                        {
-                            sectionLinks.map(({ name, link }) => (
-                                <li key={name} className="nav-items-list-item">
-                                    <Link 
-                                        activeClass="active"
-                                        to={link}
-                                        spy={true}
-                                        smooth={true}
-                                        offset={-300}
-                                        duration={700}
-                                        className="nav-items-list-item-link"
-                                    >
-                                        {name}
-                                    </Link>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                    <div className="nav-items-button">
-                        <Button text="Resume" link="http://localhost:3000/resume.pdf" />
-                    </div>
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     );
 }
 
--- src/sections/Project.tsx
+++ src/sections/Project.tsx
@@ -1,79 +1,151 @@
-import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
+import { FiGithub, FiExternalLink } from "react-icons/fi";
+import { motion } from "framer-motion";
 
 function Project() {
-    const [selected, setSelected] = useState(0);
-
-    const projects = [
+    const projectsData = [
         {
-            name: "Qualitel",
-            role: "Electrical Test Engineer",
-            url: "https://qualitel.com/",
-            positionDescription: [
-                "Collaborated with a senior engineer to develop a LabView test fixture program, referring to Python3 code.",
-                "Designed test framework components, such as the user interface, process models, and step types.",
-                "Created test sequences within the established framework.",
-                "Troubleshot and resolved issues encountered during the execution of tests in a production environment."
-            ],
+          image: "/project1.png",
+          projectName: "Pokedex",
+          projectLink: "https://netlify.com",
+          projectDescription:
+            "This is a web application that uses the PokeAPI to display information about different Pokemon, including their evolution levels and details. You can compare multiple Pokemon using a queue, and add them to your personal list using Firebase's Firestore database.",
+          projectTech: [
+            "React",
+            "Redux Toolkit",
+            "SCSS",
+            "Firebase",
+            "Typescript",
+            "PokeApi",
+          ],
+          projectExternalLinks: {
+            github: "",
+            externalLink: "",
+          },
         },
         {
-            name: "Paccar",
-            role: "Electrical Design Engineer",
-            url: "https://paccar.com/",
-            positionDescription: [
-                "Conducted comprehensive reviews and seamlessly integrated systems for a range of heavy-duty trucks, ensuring optimal performance and functionality.",
-                "Utilized technical expertise to design bespoke electrical harness systems tailored to precise customer requirements, demonstrating a meticulous attention to detail.",
-                "Played a key role in supporting the design and integration of production line systems, facilitating streamlined and efficient manufacturing processes.",
-                "Successfully managed engine and safety systems engineering projects, overseeing their seamless transition into the production line and contributing to their successful implementation.",
-            ],
-        }
+          image: "/project2.png",
+          projectName: "Realtime Chat App",
+          projectLink: "https://netlify.com",
+          projectDescription:
+            "This is a chat app made with React and Node.js that sends real-time messages and is highly scalable. It uses advanced JavaScript concepts like debouncing and is optimized for React. It has 300+ stars on GitHub and over 180k views on YouTube.",
+          projectTech: [
+            "React",
+            "Sockets",
+            "Node.js",
+            "Express",
+            "MongoDB",
+            "Styled Components",
+          ],
+          projectExternalLinks: {
+            github: "",
+            externalLink: "",
+          },
+        },
+        {
+          image: "/project3.png",
+          projectName: "Netflix App",
+          projectLink: "https://netlify.com",
+          projectDescription:
+            "I made a Netflix copy with TMBD Api. It has infinite scrolling and lets you watch movies by genre. You can also add movies to your favorites list.",
+          projectTech: [
+            "React",
+            "Node.js",
+            "Firebase",
+            "MongoDB",
+            "Express",
+            "Redux Toolkit",
+          ],
+          projectExternalLinks: {
+            github: "",
+            externalLink: "",
+          },
+        },
     ];
-
     return (
-        <div className="project" id="project">
-            <div className="title">
-                <h2>Project</h2>
-            </div>
-            <div className="project-container">
-                <ul className="exp-slider">
-                    <div className="underline"></div>
-                    {projects.map((project, index) => {
-                        return (
-                          <li key={index} className={
-                            `exp-slider-item ${index === selected && "exp-slider-item-selected"}`
-                            }
-                            onClick={() => setSelected(index)}
-                            >  
-                            <span>{project.name}</span>
-                          </li>
-                        );
-                    })}
-                </ul>
-                <div className="exp-details">
-                    <div className="exp-details-position">
-                        <h3>
-                            <span>{projects[selected].role}</span>
-                            <span className="exp-details-position-company">
-                                &nbsp;@&nbsp;
-                                <Link className="link exp-details-position-company" href={projects[selected].url}>
-                                    {projects[selected].name}{""}
-                                </Link>
-                            </span>
-                        </h3>
-                        <ul className="exp-details-list">
-                            {projects[selected].positionDescription.map((description, index) => (
-                                    <li key={index} className="exp-details">
-                                        {description}
-                                    </li>
-                                )
-                            )}
-                        </ul>
-                    </div>
+        <div className="projects" id="work">
+      <motion.div
+        className="title"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 0.6 }}
+        variants={{
+          visible: { opacity: 1, y: -50 },
+          hidden: { opacity: 0, y: 0 },
+        }}
+      >
+        <h2>Some Things I’ve Built</h2>
+      </motion.div>
+      <div className="projects-container">
+        {projectsData.map(
+          ({
+            image,
+            projectDescription,
+            projectLink,
+            projectExternalLinks,
+            projectName,
+            projectTech,
+          }) => {
+            return (
+              <motion.div
+                className="project"
+                key={projectName}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ duration: 0.6 }}
+                variants={{
+                  visible: { opacity: 1, y: -50 },
+                  hidden: { opacity: 0, y: 0 },
+                }}
+              >
+                <div className="project-image">
+                  <div className="project-image-overlay"></div>
+                  <div className="project-image-container">
+                    <Image src={image} fill alt={projectName} quality={100} />
+                  </div>
                 </div>
-            </div>
-        </div>
-
+                <div className="project-info">
+                  <p className="project-info-overline">Featured Project</p>
+                  <h3 className="project-info-title">{projectName}</h3>
+                  <div className="project-info-description">
+                    <p>{projectDescription}</p>
+                  </div>
+                  <ul className="project-info-tech-list">
+                    {projectTech.map((tech) => (
+                      <li className="project-info-tech-list-item" key={tech}>
+                        {tech}
+                      </li>
+                    ))}
+                  </ul>
+                  <ul className="project-info-links">
+                    <li className="project-info-links-item">
+                      <Link
+                        href={projectExternalLinks.github}
+                        className="project-info-links-item-link"
+                      >
+                        <FiGithub />
+                      </Link>
+                    </li>
+                    <li className="project-info-links-item">
+                      <Link
+                        href={projectExternalLinks.externalLink}
+                        className="project-info-links-item-link"
+                      >
+                        <FiExternalLink />
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </motion.div>
+            );
+          }
+        )}
+      </div>
+    </div>
     );
 }
 
