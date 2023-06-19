5d377b74a30f5333b00680af924cf9398e8c54ab
Paul Nam <poul0315@gmail.com>
2023-06-19 09:05:20 -0700

    feat(page): Added about my skills section and layout them

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,25 +4,14 @@ import Image from "next/image";
 function About() {
     return (
         <div className="about" id="about">
-            <Image 
-                src="/profile-picture.png" 
-                className="hero-profile-image"
-                width={200}
-                height={200}
-                alt="picture of Paul"
-            />
-            <h1 className="hero-title">
-                Hi my name is
-            </h1>
-            <h2 className="hero-title-large">
-                Paul Nam
-            </h2>
-            <h3 className="hero-title-large hero-title-sub">
-                I love programming.
-            </h3>
-            <p className="hero-text">
-                {`Will be added`}
-            </p>
+            <div className="grid-skill">
+                    <div className="skill-item">1</div>
+                    <div className="skill-item">1</div>
+                    <div className="skill-item">1</div>
+                    <div className="skill-item">1</div>
+                    <div className="skill-item">1</div>
+                    <div className="skill-item">1</div>
+            </div>
         </div>
     )
 }
