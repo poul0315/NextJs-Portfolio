1f776e72988d4ea3969199487c08e9608d9ba1da
Paul Nam <poul0315@gmail.com>
2023-06-19 18:44:48 -0700

    feat(page): Modified the layout of about me (skills page) and added the skillsets

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -5,12 +5,47 @@ function About() {
     return (
         <div className="about" id="about">
             <div className="grid-skill">
-                    <div className="skill-item">1</div>
-                    <div className="skill-item">1</div>
-                    <div className="skill-item">1</div>
-                    <div className="skill-item">1</div>
-                    <div className="skill-item">1</div>
-                    <div className="skill-item">1</div>
+                    <div className="skill-item">
+                        <h3>Programming Language</h3>
+                        <ul className="skill-item-details">
+                            <li>Python</li>
+                            <li>JavaScript</li>
+                            <li>Typescript</li>
+                            <li>C/C++</li>
+                            <li>Java</li>
+                            <li>Arduino</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                        </ul>
+                    </div>
+                    <div className="skill-item">
+                        <h3>Technologies & Tools</h3>
+                        <ul className="skill-item-details">
+                            <li>NextJs</li>
+                            <li>ReactJs</li>
+                            <li>NodeJs</li>
+                            <li>ExpressJs</li>
+                            <li>Bootstrap</li>
+                            <li>Tailwind</li>
+                            <li>EJS</li>
+                            <li>Openai API</li>
+                            <li>Git</li>
+                            <li>Heroku</li>
+                            <li>MongoDB</li>
+                            <li>Unix</li>
+                            <li>OpenCV</li>
+                            <li>SEAL</li>
+                            <li>Labview</li>
+                        </ul>
+                    </div>
+                    <div className="skill-item">
+                        <h3>Languages</h3>
+                        <ul className="skill-item-details">
+                            <li>Korean</li>
+                            <li>English</li>
+                            <li>Japanese</li>
+                        </ul>
+                    </div>
             </div>
         </div>
     )
